1cc5acc46a160aba45ca6a3ec44b7b684b67478d
Soporte Ceiba <cristhian4545@gmail.com>
2018-12-01 09:38:57 -0500

    estructura del proyecto

---
+++ BibliotecaUdeA/MyClass.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace BibliotecaUdeADominio
+{
+    public class MyClass
+    {
+        public MyClass()
+        {
+        }
+    }
+}
---
+++ Droid/Features/HomeActivity.cs
@@ -0,0 +1,18 @@
+﻿
+using Android.App;
+
+using Android.OS;
+
+
+namespace BibliotecaUdeA.Droid.Features
+{
+    [Activity(Label = "HomeActivity", MainLauncher = true)]
+    public class HomeActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.Main);
+        }
+    }
+}
