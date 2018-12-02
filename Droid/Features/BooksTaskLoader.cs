cdc9b01b2325f58a4040d87cb01cb7efdc49bff0
Soporte Ceiba <cristhian4545@gmail.com>
2018-12-02 16:37:47 -0500

    Se configura loaders

--- Droid/Features/BooksTaskLoader.cs
+++ Droid/Features/BooksTaskLoader.cs
@@ -1,18 +1,35 @@
 ﻿using System;
 using Android.Content;
+using BibliotecaUdeA.Business.Dtos;
+using BibliotecaUdeA.Business.Managers;
+using BibliotecaUdeA.Droid.Features.Loaders;
 using Java.Lang;
 
 namespace BibliotecaUdeA.Droid.Features
 {
     public class BooksTaskLoader : Android.Support.V4.Content.AsyncTaskLoader
     {
+        private BooksManager booksManager;
+        private string name;
+
         public BooksTaskLoader(Context context, string name) : base(context)
         {
+            this.name = name;
+            booksManager = new BooksManager();
         }
 
         public override Java.Lang.Object LoadInBackground()
         {
-            throw new NotImplementedException();
+            var response = booksManager.FetchBooksByName(name);
+            return new LoaderResponse<BaseResponse<BooksResponse>>(response);
+        }
+        protected override void OnReset()
+        {
+            CancelLoadInBackground();
+        }
+        protected override void OnStopLoading()
+        {
+            CancelLoadInBackground();
         }
     }
 }
---
+++ Droid/Features/Loaders/LoaderResponse.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using BibliotecaUdeA.Business.Dtos;
+
+namespace BibliotecaUdeA.Droid.Features.Loaders
+{
+    public class LoaderResponse : Java.Lang.Object
+    {
+        public Exception Exception { get; private set; }
+
+        public LoaderResponse() { }
+
+        public LoaderResponse(Exception exception)
+        {
+            Exception = exception;
+        }
+    }
+
+    public class LoaderResponse<TResponse> : LoaderResponse
+        {
+         
+            public TResponse Response { get; private set; }
+
+            public LoaderResponse(TResponse response) : this(response, null) { }
+
+            public LoaderResponse(Exception exception) : this(default(TResponse), exception) { }
+
+            public LoaderResponse(TResponse response, Exception exception) : base(exception)
+            {
+                Response = response;
+            }
+
+        }
+  
+}
