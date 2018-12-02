300152f90e9e9596ea3c90b0830451874ed04ff4
Soporte Ceiba <cristhian4545@gmail.com>
2018-12-02 14:31:39 -0500

    Se agrega service locator para definir en que entorno injectar

---
+++ BibliotecaUdeA/Business/DependencyInjection/Modules/RepositoriesModule.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using BibliotecaUdeA.Business.Contracts.Repositories.Remote;
+using BibliotecaUdeA.DataAcces.Repositories.Remote;
+using Ninject.Modules;
+
+namespace BibliotecaUdeA.Business.DependencyInjection.Modules
+{
+    public class RepositoriesModule : NinjectModule
+    {
+        public override void Load()
+        {
+
+#if MOCKS
+
+#else
+             Bind<IBooksRepository>().To<BooksRepository>();
+#endif
+        }
+    }
+}
---
+++ BibliotecaUdeA/DataAcces/Repositories/Remote/BooksRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using BibliotecaUdeA.Business.Contracts.Repositories.Remote;
+using BibliotecaUdeA.Business.Dtos;
+
+namespace BibliotecaUdeA.DataAcces.Repositories.Remote
+{
+    public class BooksRepository : IBooksRepository
+    {
+        public BooksResponse FetchListBooksByName(string name)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
