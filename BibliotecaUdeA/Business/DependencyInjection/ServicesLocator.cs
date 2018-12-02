dd2e12fc2d61d195cd9284b019c644c964a305d8
Soporte Ceiba <cristhian4545@gmail.com>
2018-12-02 14:20:29 -0500

    se agrega Ninject para  injectar dependencias

--- BibliotecaUdeA/Business/DependencyInjection/ServicesLocator.cs
+++ BibliotecaUdeA/Business/DependencyInjection/ServicesLocator.cs
@@ -1,10 +1,47 @@
 ﻿using System;
+using Ninject;
+using Ninject.Modules;
+using Ninject.Parameters;
+
 namespace BibliotecaUdeA.Business.DependencyInjection
 {
-    public class ServicesLocator
+    public static class ServicesLocator
     {
-        public ServicesLocator()
+        private static IKernel kernel;
+
+        public static void ConfigureKernel(NinjectModule[] modules)
         {
+            if (kernel == null)
+            {
+                kernel = new StandardKernel(modules);
+            }
+        }
+
+        public static void Inject(object target)
+        {
+            if (kernel == null)
+            {
+                throw new InvalidOperationException("El Kernel no está configurado.");
+            }
+
+            kernel.Inject(target);
+        }
+
+        public static T Get<T>(IParameter[] parameters = null)
+        {
+            if (kernel == null)
+            {
+                throw new InvalidOperationException("El Kernel no está configurado.");
+            }
+
+            if (parameters == null)
+            {
+                return kernel.Get<T>();
+            }
+            else
+            {
+                return kernel.Get<T>(parameters);
+            }
         }
     }
 }
