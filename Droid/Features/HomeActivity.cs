2a21c754db149f6b81a93225b613959a10bd0ad7
Soporte Ceiba <cristhian4545@gmail.com>
2018-12-02 11:32:06 -0500

    se agrega referencia de las vistas

--- Droid/Features/HomeActivity.cs
+++ Droid/Features/HomeActivity.cs
@@ -1,9 +1,11 @@
 ﻿
+using System;
 using System.Collections.Generic;
 using Android.App;
 
 using Android.OS;
 using Android.Support.V7.Widget;
+using Android.Widget;
 using BibliotecaUdeA.Business.Dtos;
 
 namespace BibliotecaUdeA.Droid.Features
@@ -11,14 +13,23 @@ namespace BibliotecaUdeA.Droid.Features
     [Activity(Label = "HomeActivity", MainLauncher = true)]
     public class HomeActivity : Activity
     {
-        private readonly RecyclerView rv_books;
-
+        private  RecyclerView rv_books;
+        private EditText search_box;
         private List<BookItem> books;
 
-        protected override void OnCreate(Bundle savedInstanceState)
+
+    protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             SetContentView(Resource.Layout.Main);
+
+            LoadViews();
+        }
+
+        private void LoadViews()
+        {
+            rv_books = FindViewById<RecyclerView>(Resource.Id.rv_books);
+            search_box = FindViewById<EditText>(Resource.Id.search_box);
         }
     }
 }
